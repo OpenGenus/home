d7ea42da22168ed7b62105d584c3a22b22e3adc6
Shivansh Gupta <gupta.shivansh98@gmail.com>
2018-10-29 20:27:31 +0530

    Intern Pages

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.detail import DetailView
 
+from pages.models import Interns
 # Create your views here.
 
+def internDetailView(request,username):
+	return HttpResponse("Hi")
+# class InternDetailView(DetailView):
+# 	return HttpResponse("Hi")
+
+class InternDetailView(DetailView):
+	queryset = Interns.objects.all()
+	slug_field = 'username'
