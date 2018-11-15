6e25c3d3e3598775e402e6957c4abde09d8ff217
Shivansh Gupta <gupta.shivansh98@gmail.com>
2018-11-15 07:29:47 +0530

    School and interns link

--- schools/views.py
+++ schools/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import resolve
 from django.views.generic import ListView, DetailView
 
 from .models import school
+
 # Create your views here.
 class SchoolDetailView(DetailView):
 	queryset = school.objects.all()
@@ -12,4 +14,14 @@ class SchoolDetailView(DetailView):
 	
 
 class SchoolInternView(ListView):
-	queryset= school.objects.all()
+	template_name = "schools/school_listmk.html"
+	def get_queryset(self):
+		# qs= school.objects.filter(url_endpoint="dps").first()
+		# current_url = resolve(request.path_info).url_name
+		# print(current_url)
+		school_name=self.kwargs['url_endpoint']
+		qs= school.objects.filter(url_endpoint=school_name).first()
+		print(school_name)
+		queryset = qs.latest_school.all()
+		return queryset
+	
