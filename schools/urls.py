7334a374efd6fb281f0de46edb3804ce0cae167d
Shivansh Gupta <gupta.shivansh98@gmail.com>
2018-11-19 19:03:18 +0530

    School intern and meta data

--- schools/urls.py
+++ schools/urls.py
@@ -9,8 +9,8 @@ from .views import (SchoolDetailView,SchoolInternView)
 
 app_name = 'schools' 
 urlpatterns = [
-    path('<slug:url_endpoint>/', SchoolDetailView.as_view(), name='detail'),
-    path('<slug:url_endpoint>/interns', SchoolInternView.as_view(), name='asso-interns'),
+    path('<slug:url_endpoint>/', SchoolDetailView.as_view(template_name="schools/school_detail.html"), name='detail'),
+    path('<slug:url_endpoint>/interns', SchoolDetailView.as_view(template_name="schools/school_listmk.html"), name='asso-interns'),
     # url(r'^create/$', ItemCreateView.as_view(), name='create'),
     # url(r'^(?P<pk>\d+)/update$', ItemUpdateView.as_view(), name='update'),
     # url(r'^(?P<pk>\d+)/$', ItemDetailView.as_view(), name='detail'),
